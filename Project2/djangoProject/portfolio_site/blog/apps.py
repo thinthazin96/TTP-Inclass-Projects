9acd3bfd8cd47dda77b71918f774c3feea30695d
codingsake <thinthazin.chris@gmail.com>
2022-02-02 08:30:50 -0500

    .

---
+++ Project2/djangoProject/portfolio_site/blog/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    # django 4.0 put default this line
+    # default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
---
+++ Project2/djangoProject/portfolio_site/exchange_rate/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from exchange_rate.models import exchange_rate
+
+# Register your models here.
+admin.site.register(exchange_rate)
